ac9dcd2c6c2194eed611e14616274c2fd6b4903c
MUKILTEO\1726317 <bobby.bobbyjoejoejoe@gmail.com>
2026-02-04 09:07:20 -0800

    no message

--- Party Game/Assets/LocalPlayer.cs
+++ Party Game/Assets/LocalPlayer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class LocalPlayer : MonoBehaviour
 {
@@ -15,5 +16,26 @@ public class LocalPlayer : MonoBehaviour
         playerNumber = input.playerIndex + 1;
 
         gameObject.name = "LocalPlayer" + playerNumber;
+    }
+
+    void Update()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        List<GameObject> validTargets = new List<GameObject>();
+        foreach (GameObject player in players)
+        {
+            if (player.transform.Find("Camera").Find("Canvas (1)").GetComponent<PlayerCanvasUI>().IsPaused == true)
+            {
+                validTargets.Add(player);
+            }
+        }
+        if (validTargets.Count > 0)
+        {
+            Time.timeScale = 0; // paused
+        }
+        else
+        {
+            Time.timeScale = 1; // not paused
+        }
     }
 }
